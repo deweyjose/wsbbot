d6e44c6ef6061b0cf5be78266a3476afb66c2657
Dewey Jose <rjose@teikametrics.com>
2020-06-19 19:48:11 -0400

    doc strings, some cleanup

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,24 @@
+"""
+API setup
+1. Initialize the app, db, schemas and the login_manager
+2. Create the principals
+3. Register Error Handlers
+4. Provide routes for
+    - login
+    - logout
+    - register
+    - index
+"""
+
 import os
 import random
+import uuid
+from logging.config import dictConfig
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_login import login_user, logout_user, login_required
+from flask_principal import Principal
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from api.exceptions import AlreadyExists, Unauthorized, NotFound
 from api.stock_api import stock_api
@@ -9,6 +26,23 @@ from api.user_api import user_api
 from core.authentication import login_manager
 from core.database import db
 from core.schemas import ma
+from model.user import User, user_schema
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask("WSBBOT")
 app.config.from_object(os.getenv('APP_SETTINGS', 'core.config.DevelopmentConfig'))
@@ -16,38 +50,59 @@ db.init_app(app)
 ma.init_app(app)
 login_manager.init_app(app)
 
+principals = Principal(app)
+
+
+def wrapp_error(status_code, payload):
+    """
+    Wrap all of our exception responses in a JSON object
+    """
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
+
 
 @app.errorhandler(Exception)
 def handle_unknown(error):
+    """
+    Catch any unknown exceptions.
+    Try to transform errors with a name and code.
+    If those attributes do not exist default to 500/unknown error
+    """
     app.logger.error(error)
-    response = jsonify({"message": "unknown error"})
-    response.status_code = 500
-    return response
+    return wrapp_error(500 if not error.code else error.code,
+                       {"message": "unknown error" if not error.name else error.name})
 
 
 @app.errorhandler(NotFound)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+def handl_not_found(error):
+    """
+    Catch and transform any NotFound errors into JSON format.
+    """
+    return wrapp_error(error.to_dict(), error.status_code)
 
 
 @app.errorhandler(Unauthorized)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+def handle_unauthorized(error):
+    """
+    Catch and transform any Unauthorized errors into JSON format.
+    """
+    return wrapp_error(error.to_dict(), error.status_code)
 
 
 @app.errorhandler(AlreadyExists)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+def handle_already_exists(error):
+    """
+    Catch and transform any AlreadyExists errors into JSON format.
+    """
+    return wrapp_error(error.to_dict(), error.status_code)
 
 
 @app.route("/", methods=["GET"])
 def index():
+    """
+    The root route. Returns a random popular WSB phrase.
+    """
     terms = [
         "Stonks only go up",
         "Pump and dump",
@@ -57,6 +112,75 @@ def index():
         "Don't fight the fed"
     ]
     return terms[random.randrange(0, len(terms))]
+
+
+def authenticate_user(email, password):
+    """
+    Authenticate the User:
+        1. Fetch the user from the database
+        2. Make sure the password hashes match
+    """
+    user = User.query.filter_by(email=email).first()
+
+    if user is None or not check_password_hash(user.password, password):
+        raise Unauthorized("Unauthorized")
+
+    return user
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    used by flask-login to pull the user from the database **if** the user has previously authenticated and is "logged in"
+    """
+    return User.query.get(user_id)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    Login route:
+    1. authenticate the user
+    2. login_user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = request.form.get('remember', False)
+
+    user = authenticate_user(email, password)
+
+    login_user(user, remember)
+
+    return jsonify(user_schema.dump(user))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """
+    Log the user out so session state is cleared.
+    """
+    logout_user()
+    return jsonify({"messaged": "logged out"})
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+    """
+    Register the user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        raise AlreadyExists(f"A user has already registered with {email}")
+
+    new_user = User(id=uuid.uuid4(), email=email, password=generate_password_hash(password))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(new_user))
 
 
 app.register_blueprint(user_api)
--- api/user_api.py
+++ api/user_api.py
@@ -1,67 +1,11 @@
-import uuid
+from flask import jsonify, Blueprint
+from flask_login import login_required
 
-from flask import jsonify, request, Blueprint
-from flask_login import login_user, login_required, logout_user
-from werkzeug.security import generate_password_hash, check_password_hash
-
-from api.exceptions import NotFound, Unauthorized, AlreadyExists
-from core.authentication import login_manager
+from api.exceptions import NotFound
 from core.database import db
-from model.user import User
-from model.user import user_schema, users_schema
+from model.user import User, user_schema, users_schema
 
 user_api = Blueprint('user_api', __name__)
-
-
-@login_manager.user_loader
-def load_user(account_id):
-    return User.query.get(account_id)
-
-
-def authenticate_user(email, password):
-    user = User.query.filter_by(email=email).first()
-
-    if user is None or not check_password_hash(user.password, generate_password_hash(password)):
-        raise Unauthorized("Unauthorized")
-    return True
-
-
-@user_api.route("/user/register", methods=["GET", "POST"])
-def register_user():
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()
-    if user:
-        raise AlreadyExists(f"A user has already registered with {email}")
-
-    new_user = User(id=uuid.uuid4(), email=email, password=generate_password_hash(password))
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify(user_schema.dump(user))
-
-
-@user_api.route("/user/login", methods=["POST"])
-def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
-        raise Unauthorized("invalid login")
-
-    login_user(user, remember)
-
-    return jsonify(user_schema.dump(user))
-
-
-@user_api.route("/user/logout")
-@login_required
-def logout():
-    logout_user()
-    return jsonify({"messaged":"logged out"})
 
 
 @user_api.route("/user", methods=["GET"])
